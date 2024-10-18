0eddea079c7a506d18934d82e5240ba518cd0a9e
Deepali Yadav <dy19648@gmail.com>
2024-10-18 16:42:13 +0530

    changes

--- main.py
+++ main.py
@@ -51,10 +51,14 @@ def read_item(url: Optional[str] = None):
             for cell in th:
                 head.append(cell.get_text(strip=True))
             rows.append(arr)
+
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
     chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
     chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    chrome_options.add_argument("--disable-gpu") 
+    chrome_options.add_argument("--remote-debugging-port=9222") 
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
